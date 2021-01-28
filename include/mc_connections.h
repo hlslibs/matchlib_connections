0d6dea004839a3614d36250fb6296087fa162018
David Burnette <dgb@orw-dgb-r78.wv.mentorg.com>
2021-01-28 15:19:32 -0800

    Changes in 1.2.3 (see doc/matchlib_connections_relnotes.pdf for details)

--- include/mc_connections.h
+++ include/mc_connections.h
@@ -4,9 +4,9 @@
  *                                                                        *
  *  Software Version: 1.2                                                 *
  *                                                                        *
- *  Release Date    : Thu Jan 28 15:10:47 PST 2021                        *
+ *  Release Date    : Thu Jan 28 15:19:09 PST 2021                        *
  *  Release Type    : Production Release                                  *
- *  Release Build   : 1.2.2                                               *
+ *  Release Build   : 1.2.3                                               *
  *                                                                        *
  *  Copyright , Mentor Graphics Corporation,                     *
  *                                                                        *
